1ca07a08b6a9dbc7b01b14f4dd551e6a1f291a89
EzzatELshazly <65233473+EzzatELshazly@users.noreply.github.com>
2025-11-03 22:53:18 +0200

    Configure Azure backend for Terraform state management
    Added backend configuration for Azure storage to manage Terraform state.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,13 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-aks-microservice"
+    storage_account_name = "terraformsatfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
   required_version = ">= 1.0"
-  
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
